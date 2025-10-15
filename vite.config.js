517f2cc03dc8ba6504dfe2f136eb2fde2badecd2
Lucas Sousa <223877261+Lucas-088@users.noreply.github.com>
2025-10-15 17:31:34 -0300

    update

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,21 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
-  base: '/', 
+  base: '/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        sobre: resolve(__dirname, 'sobre.html'),
+        projetos: resolve(__dirname, 'projetos.html'),
+        voluntariado: resolve(__dirname, 'voluntariado.html'),
+        doacoes: resolve(__dirname, 'doacoes.html'),
+        transparencia: resolve(__dirname, 'transparencia.html'),
+        blog: resolve(__dirname, 'blog.html'),
+        contato: resolve(__dirname, 'contato.html'),
+        dashboard: resolve(__dirname, 'dashboard.html'),
+      }
+    }
+  }
 })
